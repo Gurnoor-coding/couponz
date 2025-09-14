93042f9d286bc133787309f228b48f0e0bb62335
Gurnoor Singh <gurnos1@uci.edu>
2025-09-13 17:43:59 -0700

    Another ooone but3234567

--- couponz-backend/server.js
+++ couponz-backend/server.js
@@ -10,7 +10,9 @@ const { SESClient, SendEmailCommand } = require('@aws-sdk/client-ses');
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+
+app.use(cors({ origin: '*' })); 
+
 app.use(express.json());
 
 // DynamoDB setup
